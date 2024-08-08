ff8f7fe71c702a04c3842d78a22aa16323ddd70d
Enes Emre AYDOGAN <eeay0@protonmail.com>
2024-08-08 18:23:02 +0300

    cleaning

--- fish/functions/fzf_mine.fish
+++
@@ -1,7 +0,0 @@
-function n
-    set directory "/home/eeay/vaults"
-    set folder (fd -a -t d --search-path "$directory" | sed "s|$directory/||" | fzf)
-    if test -n "$folder"
-        cd "$directory/$folder"
-    end
-end
